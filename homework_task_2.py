5079d6986634a5e58597fe669d4a0d7c6d09673f
4attye <167115532+4attye@users.noreply.github.com>
2024-08-31 20:00:44 +0300

    ДЗ_3

--- homework_task_2.py
+++ homework_task_2.py
@@ -4,14 +4,8 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 import requests
 
-client = MongoClient(
-    "mongodb+srv://4attye:upEIrWRmbcF1WVtn@clusters.s150f.mongodb.net/",
-    server_api=ServerApi('1')
-)
-
+client = MongoClient("mongodb+srv://4attye:upEIrWRmbcF1WVtn@clusters.s150f.mongodb.net/",server_api=ServerApi('1'))
 db = client.qoutes
-
-
 url = "http://quotes.toscrape.com"
 qoutes_data = []
 authors_data = []
@@ -26,7 +20,19 @@ def get_author_details(author_url):
     return born_date, born_location, description
 
 
-def get_qoutes ():
+def write_json():
+    with open('authors.json', 'w', encoding="utf-8") as f:
+        json.dump(authors_data, f, indent=4)
+    with open('quotes.json', 'w', encoding="utf-8") as f:
+        json.dump(qoutes_data, f, ensure_ascii=False, indent=4)
+
+
+def insert_data():
+    db.authors.insert_many(authors_data)
+    db.qoutes.insert_many(qoutes_data)
+
+
+def main ():
     page = 1
     while True:
         response = requests.get(f"{url}/page/{page}/")
@@ -58,16 +64,10 @@ def get_qoutes ():
                 })
         page += 1
 
+    write_json()
+    insert_data()
 
-def insert_data():
-    db.authors.insert_many(authors_data)
-    db.qoutes.insert_many(qoutes_data)
 
 if __name__ == '__main__':
-    get_qoutes ()
-    with open('authors.json', 'w', encoding="utf-8") as f:
-        json.dump(authors_data, f, indent=4)
-    with open('quotes.json', 'w', encoding="utf-8") as f:
-        json.dump(qoutes_data, f, ensure_ascii=False, indent=4)
-    insert_data()
+    main ()
 
